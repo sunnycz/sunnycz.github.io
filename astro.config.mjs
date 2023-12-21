dbec6b2601c10803afe10a1f19074640d4da02cd
Pavel Paranin <pavel@paran.in>
2023-12-21 13:49:18 +0100

    CD: added Astro deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://sunnycz.github.io',
   integrations: [mdx(), tailwind()]
 });
