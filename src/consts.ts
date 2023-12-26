37e2bcb5b67d1b631fbd3fc95baac7e062d6f38c
Pavel Paranin <pavel@paran.in>
2023-12-26 21:05:12 +0100

    fix: favicon path

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = ' Ravil Itikeev';
 export const SITE_DESCRIPTION = 'Ravil Itikeev - Digital Designer';
 export const SITE_LOGO = '../logo.svg';
-export const SITE_FAVICON = '../favicon.ico';
+export const SITE_FAVICON = '/favicon.ico';
